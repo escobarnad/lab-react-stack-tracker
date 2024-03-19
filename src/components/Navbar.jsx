d73523c34a36c6535a825f9341cfff38c87ac617
Nadia Escobar <escobarnad@gmail.com>
2024-03-19 17:10:41 +0100

    Clean

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,5 @@
-import "../App.css";
-
 function Navbar() {
   return <nav>Navbar</nav>;
-  return <nav className="navbar">StackTracker</nav>;
 }
 
 export default Navbar;
--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,29 +1,7 @@
-import "../App.css";
-import { Link, NavLink, Route, Routes, useParams } from 'react-router-dom'
-
-function CompanyPage({ companies }) {
-  const { companySlug } = useParams();
-  const matchMaker = companies.find(company => company.slug === companySlug)
+function CompanyPage() {
   return (
     <div>
-       <h1>Company profile</h1>
-      <div className="containerTechs">
-        <a href={matchMaker.website} target='_blank' rel='noreferrer'><img className='logoCompany' src={matchMaker.logo} /></a>
-        <div className="details">
-          <p className="title">{matchMaker.name}</p>
-          <p>{matchMaker.description}</p>
-        </div>
-      </div>
-      {matchMaker.techStack.map((tech) => {
-        return (
-          <div>
-            <Link to={`/tech/${tech.slug}`}>
-              <p>{tech.name}</p>
-              <img src={tech.image} height={100} />
-            </Link>
-          </div>
-        )
-      })}
+      <h1>CompanyPage</h1>
     </div>
   );
 }
