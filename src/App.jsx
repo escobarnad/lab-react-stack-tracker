02ef85f051b1aae73bd915373f16a148e01db446
Nadia Escobar <escobarnad@gmail.com>
2024-03-20 09:51:38 +0100

    Iteration 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import companiesJSON from "./companies.json";
+import techJSON from "./technologies.json";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CompanyPage from "./pages/CompanyPage";
+import TechnologyPage from "./pages/TechnologyPage";
 
 function App() {
+  const [companies, setCompanies] = useState(companiesJSON);
+  const [tech, setTech] = useState(techJSON);
+
   return (
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/companie/:companySlug" element={<CompanyPage />} />
+        <Route path="/tech/:slug" element={<TechnologyPage />} />
+      </Routes>
     </div>
   );
 }
